cec4febeeb3e1a43a6ddd242740ae467822b4541
Alexandre Philibert <alexandre.philibert@ik.me>
2023-07-02 14:12:14 +0200

    grid layout with title offset

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -35,7 +35,7 @@
 		{
 			title: 'Développeur logiciel',
 			description: [
-				'Développement de nouvelles fonctionnalités sur la plateforme IntrepidKnowledge'
+				'Développement de nouvelles fonctionnalités sur la plateforme IntrepidKnowledge.'
 			],
 			technologies: ['React', 'NestJS', 'CSS', 'Terraform', 'Ansible', 'OpenStack'],
 			company: {
@@ -47,7 +47,9 @@
 		},
 		{
 			title: 'Travail de diplôme Technicien ES',
-			description: ["Migration de l'API PHP d'IntrepidKnowledge vers Node.js"],
+			description: [
+				"Projet de migration de l'API PHP d'IntrepidKnowledge vers Node.js. Début de la migration de la partie authentification, envoi d'email et permissions."
+			],
 			technologies: ['Node.js', 'NestJS', 'OAuth2'],
 			company: {
 				name: 'WorkStreams',
@@ -58,7 +60,7 @@
 		},
 		{
 			title: 'Stage à WorkStreams',
-			description: ['Migration du frontend de la plateforme IntrepidKnowledge vers React'],
+			description: ['Migration du frontend de la plateforme IntrepidKnowledge vers React.'],
 			technologies: ['React', 'TypeScrit', 'CSS', 'React-Query'],
 			company: {
 				name: 'WorkStreams',
@@ -70,7 +72,7 @@
 		{
 			title: 'Informaticien CFC à WorkStreams',
 			description: [
-				'Implémentation de nouvelles fonctionnalités sur la plateforme IntrepidKnowledge'
+				'Implémentation de nouvelles fonctionnalités sur la plateforme IntrepidKnowledge.'
 			],
 			technologies: ['Javascript', 'PHP', 'CSS'],
 			company: {
@@ -82,7 +84,9 @@
 		},
 		{
 			title: 'Stage à Nagravision',
-			description: ['Analyse de spécification et implémentation de processus de commandes interne'],
+			description: [
+				'Analyse de spécification et implémentation de processus de commandes interne.'
+			],
 			technologies: ['Nintex', 'K2'],
 			company: {
 				name: 'Nagravision',
@@ -137,7 +141,7 @@
 				"Application de suivi des heures de travail, suivi des chantiers et de partage de fichiers pour les employés de l'entreprise.",
 			url: null,
 			image: '/imgs/drive_bocherens_com.png',
-			technologies: ['Laravel', 'React', 'MUI', 'NGINX']
+			technologies: ['Laravel', 'React', 'TypeScript', 'MUI', 'NGINX']
 		},
 		{
 			name: 'pinceau.ch',
@@ -158,152 +162,168 @@
 	];
 </script>
 
-<main class="flex flex-col w-full items-center space-y-8 my-6">
-	<div class="max-w-2xl w-full">
-		<h2 class="text-2xl mb-6 font-bold">Expériences Professionnelles</h2>
-		<ol class="space-y-8">
-			{#each professionalExperiences as experience}
-				<li class="grid grid-cols-6">
-					<div class="col-span-1">
-						<span class="text-slate-400 text-sm">
-							<span>{experience.startYear}</span> - <span>{experience.endYear ?? 'Actuel.'}</span>
-						</span>
-					</div>
-					<div class="col-span-5">
-						<h2 class="font-medium text-lg mb-1">{experience.title}</h2>
-						<ul class="flex flex-wrap mb-1 -ml-1">
-							{#each experience.technologies as technology}
-								<li
-									class="m-1 py-0.5 px-2 font-medium rounded-lg text-cerulean-blue-800 bg-gradient-to-tr from-scooter-400 to-scooter-200 select-none"
-								>
-									{technology}
-								</li>
-							{/each}
-						</ul>
-						<p class="text-justify text-base/8 text-oslo-gray-300">{experience.description}</p>
-						<a href={experience.company.url} class="flex items-center font-medium group space-x-2">
-							<span>
-								{experience.company.name}
+<div class="flex justify-center">
+	<main class="flex flex-col w-full max-w-3xl items-center space-y-10 my-6">
+		<div class="grid grid-cols-6 w-full gap-8">
+			<div class="col-start-2 col-span-5">
+				<h2 class="text-2xl mb-2 font-medium uppercase">Expériences Professionnelles</h2>
+				<hr class="border-scooter-200 border-b" />
+			</div>
+			<ol class="contents">
+				{#each professionalExperiences as experience}
+					<li class="contents">
+						<div class="col-span-1">
+							<span class="text-slate-400 text-sm">
+								<span>{experience.startYear}</span> - <span>{experience.endYear ?? 'Actuel.'}</span>
 							</span>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								width="14"
-								height="14"
-								viewBox="0 0 24 24"
-								fill="none"
-								stroke="currentColor"
-								stroke-width="2"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								class="text-oslo-gray-200 group-hover:inline-block hidden mb-0.5"
-								><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" /><polyline
-									points="15 3 21 3 21 9"
-								/><line x1="10" x2="21" y1="14" y2="3" /></svg
-							>
-						</a>
-					</div>
-				</li>
-			{/each}
-		</ol>
-	</div>
-
-	<div class="max-w-2xl w-full">
-		<h2 class="text-2xl mb-6 font-bold">Formations</h2>
-		<ol class="space-y-8">
-			{#each educations as education}
-				<li class="grid grid-cols-6 group">
-					<div class="col-span-1 pt-2.5">
-						<span class="text-slate-400 text-sm">
-							<span>{education.startYear}</span> - <span>{education.endYear}</span>
-						</span>
-					</div>
-					<a
-						href={education.url}
-						class="col-span-5 px-3 py-2 cursor-pointer hover:bg-cerulean-blue-900 bg-opacity-50 rounded-md border border-transparent hover:border-scooter-900"
-					>
-						<span class="flex justify-between">
-							<h2 class="font-medium text-lg mb-2">{education.name}</h2>
-							<svg
-								xmlns="http://www.w3.org/2000/svg"
-								width="22"
-								height="22"
-								viewBox="0 0 24 24"
-								fill="none"
-								stroke="currentColor"
-								stroke-width="2"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								class="text-oslo-gray-200 group-hover:inline-block hidden"
-								><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" /><polyline
-									points="15 3 21 3 21 9"
-								/><line x1="10" x2="21" y1="14" y2="3" /></svg
-							>
-						</span>
-						<div class="font-medium">{education.school.name}</div>
-					</a>
-				</li>
-			{/each}
-		</ol>
-	</div>
-
-	<div class="max-w-2xl w-full">
-		<h2 class="text-2xl mb-6 font-bold">Projets</h2>
-		<ol class="space-y-8">
-			{#each projects as project}
-				<li>
-					<ConditionalWrapper
-						tag="a"
-						wrap={!!project.url}
-						href={project.url}
-						class="inline-block cursor-pointer hover:bg-cerulean-blue-900 bg-opacity-50 rounded-md border border-transparent hover:border-scooter-900 group"
-					>
-						<div class="grid grid-cols-8 grid-rows-1 space-x-6 p-3">
-							<div
-								class="rounded-lg col-span-3 border border-scooter-900 row-span-1 h-full overflow-hidden"
-							>
-								<img src={project.image} alt={project.name} class="object-cover w-full h-full" />
-							</div>
-							<div class="col-span-5">
-								<span class="flex justify-between">
-									<h2 class="font-medium text-xl mb-1">{project.name}</h2>
-									{#if !!project.url}
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											width="22"
-											height="22"
-											viewBox="0 0 24 24"
-											fill="none"
-											stroke="currentColor"
-											stroke-width="2"
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											class="text-oslo-gray-200 group-hover:inline-block hidden"
-											><path
-												d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"
-											/><polyline points="15 3 21 3 21 9" /><line
-												x1="10"
-												x2="21"
-												y1="14"
-												y2="3"
-											/></svg
-										>
-									{/if}
-								</span>
-								<ul class="flex flex-wrap mb-1 -ml-1">
-									{#each project.technologies as technology}
-										<li
-											class="m-1 py-0.5 px-2 font-medium rounded-lg text-cerulean-blue-800 bg-gradient-to-tr from-scooter-400 to-scooter-200 select-none"
-										>
-											{technology}
-										</li>
-									{/each}
-								</ul>
-								<p class="text-oslo-gray-300 text-justify">{project.description}</p>
-							</div>
 						</div>
-					</ConditionalWrapper>
-				</li>
-			{/each}
-		</ol>
-	</div>
-</main>
+						<div class="col-span-5">
+							<h2 class="font-medium text-lg mb-1">{experience.title}</h2>
+							<ul class="flex flex-wrap mb-1.5 -ml-1">
+								{#each experience.technologies as technology}
+									<li
+										class="m-1 py-0.5 px-2 font-medium rounded-lg text-cerulean-blue-800 bg-gradient-to-tr from-scooter-400 to-scooter-200 select-none"
+									>
+										{technology}
+									</li>
+								{/each}
+							</ul>
+							<p class="text-justify text-base/6 text-oslo-gray-300 mb-1.5">
+								{experience.description}
+							</p>
+							<a
+								href={experience.company.url}
+								class="flex items-center font-medium group space-x-2 max-w-min"
+							>
+								<span>
+									{experience.company.name}
+								</span>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="14"
+									height="14"
+									viewBox="0 0 24 24"
+									fill="none"
+									stroke="currentColor"
+									stroke-width="2"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									class="text-oslo-gray-200 group-hover:visible invisible mb-0.5"
+									><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" /><polyline
+										points="15 3 21 3 21 9"
+									/><line x1="10" x2="21" y1="14" y2="3" /></svg
+								>
+							</a>
+						</div>
+					</li>
+				{/each}
+			</ol>
+		</div>
+
+		<div class="grid grid-cols-6 gap-8 w-full">
+			<div class="col-start-2 col-span-5">
+				<h2 class="text-2xl mb-2 font-medium uppercase">Formations</h2>
+				<hr class="border-scooter-200 border-b" />
+			</div>
+			<ol class="contents">
+				{#each educations as education}
+					<li class="contents">
+						<div class="col-span-1 pt-2.5">
+							<span class="text-slate-400 text-sm">
+								<span>{education.startYear}</span> - <span>{education.endYear}</span>
+							</span>
+						</div>
+						<a
+							href={education.url}
+							class="col-span-5 px-3 py-2 -mx-3 -my-2 cursor-pointer hover:bg-cerulean-blue-900 bg-opacity-50 rounded-md border border-transparent hover:border-scooter-900 group"
+						>
+							<span class="flex justify-between">
+								<h2 class="font-medium text-lg mb-2">{education.name}</h2>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="22"
+									height="22"
+									viewBox="0 0 24 24"
+									fill="none"
+									stroke="currentColor"
+									stroke-width="2"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									class="text-oslo-gray-200 group-hover:inline-block hidden"
+									><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" /><polyline
+										points="15 3 21 3 21 9"
+									/><line x1="10" x2="21" y1="14" y2="3" /></svg
+								>
+							</span>
+							<div class="font-medium">{education.school.name}</div>
+						</a>
+					</li>
+				{/each}
+			</ol>
+		</div>
+
+		<div class="grid grid-cols-6 gap-8 w-full">
+			<div class="col-start-2 col-span-5">
+				<h2 class="col-span-6 text-2xl mb-2 font-medium uppercase">Projets</h2>
+				<hr class="border-scooter-200 border-b" />
+			</div>
+			<ol class="contents">
+				{#each projects as project}
+					<li class="col-start-2 col-span-5">
+						<ConditionalWrapper
+							tag="a"
+							wrap={!!project.url}
+							href={project.url}
+							class="inline-block cursor-pointer hover:bg-cerulean-blue-900 bg-opacity-50 rounded-md border border-transparent hover:border-scooter-900 group"
+						>
+							<div class="grid grid-cols-8 grid-rows-1 space-x-6 p-3">
+								<div
+									class="rounded-lg col-span-3 border border-scooter-900 row-span-1 h-full overflow-hidden"
+								>
+									<img src={project.image} alt={project.name} class="object-cover w-full h-full" />
+								</div>
+								<div class="col-span-5">
+									<span class="flex justify-between">
+										<h2 class="font-medium text-xl mb-1.5">{project.name}</h2>
+										{#if !!project.url}
+											<svg
+												xmlns="http://www.w3.org/2000/svg"
+												width="22"
+												height="22"
+												viewBox="0 0 24 24"
+												fill="none"
+												stroke="currentColor"
+												stroke-width="2"
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												class="text-oslo-gray-200 group-hover:inline-block hidden"
+												><path
+													d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"
+												/><polyline points="15 3 21 3 21 9" /><line
+													x1="10"
+													x2="21"
+													y1="14"
+													y2="3"
+												/></svg
+											>
+										{/if}
+									</span>
+									<ul class="flex flex-wrap mb-1 -ml-1">
+										{#each project.technologies as technology}
+											<li
+												class="m-1 py-0.5 px-2 font-medium rounded-lg text-cerulean-blue-800 bg-gradient-to-tr from-scooter-400 to-scooter-200 select-none"
+											>
+												{technology}
+											</li>
+										{/each}
+									</ul>
+									<p class="text-oslo-gray-300 text-base/6 text-justify">{project.description}</p>
+								</div>
+							</div>
+						</ConditionalWrapper>
+					</li>
+				{/each}
+			</ol>
+		</div>
+	</main>
+</div>
